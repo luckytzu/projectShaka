19825d592c77f6d8ca752def10b4a1138265aa3a
luckytzu <tatindylan@gmail.com>
2026-07-27 18:38:36 +0200

    Ajout du favicon et configuration des métadonnées

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "JDR Shaka",
   description: "Jeu de rôle de shaka",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
